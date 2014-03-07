8c3f12d74674240ac089f6af4b0f3b7728604c73
Emmanuel Freard <e.freard@vi-live.Fr>
2014-03-07 19:28:24 +0100

    buffer loader testing, need more error testing

--- tests/tests.js
+++ tests/tests.js
@@ -1,134 +1,124 @@
+var assert = chai.assert;
+
 var audioContext = new webkitAudioContext();
-var targetNode = audioContext.destination;
-var player = null;
-var audioBuffer = null;
-var currentTime = null;
 
-function assert(expr, msg) {
-  if (!expr) throw new Error(msg || 'failed');
-}
-
-function floatToInt(f) {
-  return Math.round(f * 1000);
-}
-
-describe("Sound : sound.wav", function() {
+describe("Load some sounds: synth.wav and sound.wav", function() {
   var self = this;
 
-  var sound = {
-    audioBuffer: {
-      duration: 185.75672916666667,
-      gain: 1,
-      length: 8916323,
-      numberOfChannels: 2,
-      sampleRate: 48000
-    }
-  };
+  var myAudioBuffers = [{ //synth.wav
+    duration: 1.7874829931972789,
+    gain: 1,
+    length: 78828,
+    numberOfChannels: 1,
+    sampleRate: 44100
+  }, { //sound.wav
+    duration: 3.633605442176871, 
+    gain: 1,
+    length: 160242,
+    numberOfChannels: 2,
+    sampleRate: 44100
+  }];
 
   var myTimeout = 200000;
 
-  it('My sound was loaded !', function(done) {
+  it('My sound was loaded with "load"', function(done) {
     this.timeout(myTimeout);
     bufferLoader.load('../node_modules/snd-dep/sound.wav', function(audioBuffer) {
-      self.audioBuffer = audioBuffer;
-      assert(typeof(self.audioBuffer) === "object", "AudioBuffer is not an object");
-      assert(self.audioBuffer.length === sound.audioBuffer.length, "AudioBuffer length is not correct");
+        console.log(audioBuffer);
+      assert.isObject(audioBuffer, 'audioBuffer is not an object');
+      done();
+    }, audioContext);
+  });
+  
+  it('My sound was loaded with "loadBuffer"', function(done) {
+    this.timeout(myTimeout);
+    bufferLoader.loadBuffer('../node_modules/snd-dep/sound.wav', function(audioBuffer) {
+      assert.isObject(audioBuffer, 'audioBuffer is not an object');
       done();
 
     }, audioContext);
   });
-
-  it('my player start with offset at 0', function(done) {
+ 
+  /*
+  If an order notion will be developped
+  it('My sounds were loaded with "loadEach"', function(done) {
     this.timeout(myTimeout);
-    self.player = createPlayer(self.audioBuffer, audioContext);
-    self.player.connect(self.targetNode); // unconnected by default
-    var offset = self.player.start();
-    self.currentTime = offset;
-    assert(offset === 0, "offset first start is not equal to 0");
-    done();
-  });
-
-  it('my player pause', function(done) {
-    this.timeout(myTimeout);
-
-    setTimeout(function() {
-      self.player.pause();
-      var time = self.player.startPosition;
-      self.currentTime = time;
-      time = floatToInt(time);
-
-      //TODO Find an other way to test elapsed time 
-      if (time < 500 || time > 530) {
-        assert(false, "offset of pause is not between 1.0 and 1.1");
-      }
-
-      done();
-    }, 500);
-  });
-
-  it('my player restart', function(done) {
-    this.timeout(myTimeout);
-
-    setTimeout(function() {
-      var offset = self.player.start();
-
-      assert(self.currentTime === offset, "offset of start doesn't match with pause")
-
-      done();
-    }, 1000);
-  });
-
-  it('my player is ended', function(done) {
-    this.timeout(myTimeout);
-
-    setTimeout(function() {
-      self.player.seek(Math.floor(sound.audioBuffer.duration));
-      self.player.on("ended", function() {
+    var count = 0;
+    bufferLoader.loadEach(['./synth.wav', '../node_modules/snd-dep/sound.wav'], function(audioBuffer) {
+      assert.equal(myAudioBuffers[count].length, audioBuffer.length, 'buffer' + count + ' is not ok');
+      count++;
+      if(count == 2)
         done();
-      });
-    }, 1500);
+    }, audioContext);
   });
-
-  it('my player re-start after ended', function(done) {
+  
+  it('My sounds were loaded with "loadEach" in reverse order', function(done) {
     this.timeout(myTimeout);
-
-    setTimeout(function() {
-      var offset = self.player.start();
-      assert(offset === 0, "offset first start is not equal to 0");
-      done();
-    }, 3000);
+    var count = 1;
+    bufferLoader.loadEach(['../node_modules/snd-dep/sound.wav', './synth.wav'], function(audioBuffer) {
+      assert.equal(myAudioBuffers[count].length, audioBuffer.length, 'buffer' + count + ' is not ok');
+      count--;
+      if(count == -1)
+        done();
+    }, audioContext);
+  });*/
+  
+  it('My sounds were loaded with "loadEach" in reverse order', function(done) {
+    this.timeout(myTimeout);
+    var count = 0;
+    bufferLoader.loadEach(['../node_modules/snd-dep/sound.wav', './synth.wav'], function(audioBuffer) {
+      assert.isObject(audioBuffer, 'audioBuffer is not an object');
+      count++;
+      if(count == 2)
+        done();
+    }, audioContext);
   });
-
-  it('seek with overflow value', function(done) {
+  
+  it('My sounds were loaded with "loadAll"', function(done) {
     this.timeout(myTimeout);
-
-    setTimeout(function() {
-      var seek = self.player.seek((Math.floor(sound.audioBuffer.duration) + 10));
-      var time = self.player.startPosition;
-      var overflow = (Math.floor(sound.audioBuffer.duration) + 10) - sound.audioBuffer.duration;
-      assert(time === seek, "startPosition is not equal to seek return position");
-
-      //maybe it's normal
-      //assert(time === overflow, "overflow is not equal to startposition");
+    bufferLoader.loadAll(['./synth.wav', '../node_modules/snd-dep/sound.wav'], function(audioBuffers) {
+      assert.equal(myAudioBuffers[0].length, audioBuffers[0].length, 'buffer1 is not ok');
+      assert.equal(myAudioBuffers[1].length, audioBuffers[1].length, 'buffer2 is not ok');
       done();
-
-    }, 3500);
+    }, audioContext);
   });
-
-  it('set gain', function(done) {
+  
+  it('My sounds were loaded with "loadAll" in reverse order', function(done) {
     this.timeout(myTimeout);
-
-    setTimeout(function() {
-      var myGain = 0.4;
-      self.player.setGain(myGain);
-
-      assert(myGain.toString() === self.player.gain.toString(), myGain.toString() + " is not equal to " + self.player.gain.toString());
-
-      assert((myGain * myGain).toString() === self.player.gainNode.gain.value.toString(), (myGain * myGain).toString() + " is not equal to gainNode.gain.value : " + self.player.gainNode.gain.value.toString());
-
+    bufferLoader.loadAll(['../node_modules/snd-dep/sound.wav', './synth.wav'], function(audioBuffers) {
+      assert.equal(myAudioBuffers[0].length, audioBuffers[1].length, 'buffer1 is not ok');
+      assert.equal(myAudioBuffers[1].length, audioBuffers[0].length, 'buffer2 is not ok');
       done();
-
-    }, 4000);
+    }, audioContext);
+  });
+  
+  it('My sounds were loaded with "load"', function(done) {
+    this.timeout(myTimeout);
+    bufferLoader.load(['./synth.wav', '../node_modules/snd-dep/sound.wav'], function(audioBuffers) {
+      assert.equal(myAudioBuffers[0].length, audioBuffers[0].length, 'buffer1 is not ok');
+      assert.equal(myAudioBuffers[1].length, audioBuffers[1].length, 'buffer2 is not ok');
+      done();
+    }, audioContext);
+  });
+  
+  it('My sounds were loaded with "load" in reverse order', function(done) {
+    this.timeout(myTimeout);
+    bufferLoader.load(['../node_modules/snd-dep/sound.wav', './synth.wav'], function(audioBuffers) {
+      assert.equal(myAudioBuffers[0].length, audioBuffers[1].length, 'buffer1 is not ok');
+      assert.equal(myAudioBuffers[1].length, audioBuffers[0].length, 'buffer2 is not ok');
+      done();
+    }, audioContext);
+  });
+  
+  it('My sound is not correct, error was detected', function(done) {
+    this.timeout(myTimeout);
+    try {
+      bufferLoader.load('./nothing.txt', function(audioBuffer) {
+      console.log(audioBuffer);
+      }, audioContext);
+    } catch(e) {
+      done();
+    }
   });
 
 });
