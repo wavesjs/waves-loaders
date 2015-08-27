e78c5622575e2bb3707ed75d9449c7a858438817
ouhouhsami <samuel.goldszmidt@gmail.com>
2015-08-27 18:04:21 +0200

    Add coverage

--- bin/scripts.js
+++ bin/scripts.js
@@ -8,6 +8,12 @@ var fse = require('fs-extra');
 var nodeWatch = require('node-watch');
 var uglifyJS = require('uglify-js');
 
+var clc = require('cli-color');
+var fs = require("fs");
+var minimist = require('minimist');
+var pad = require('node-string-pad');
+var pkg = require('../package.json');
+
 // CONFIG
 // -----------------------------------------------
 var srcDir = 'es6';
@@ -48,6 +54,9 @@ switch (command) {
     break;
   case '--transpile':
     transpileAll();
+    break;
+  case '--cover-report':
+    coverReport();
     break;
 }
 
@@ -139,3 +148,37 @@ function transpile(src) {
     });
   });
 }
+
+// Cover report
+function coverReport() {
+  'use strict';
+
+  var argv = minimist(process.argv.slice(3));
+  var chunks = [];
+  var uncovered = clc.red.bold;
+  var covered = clc.green;
+  var f = fs.readFileSync(argv['i']);
+  var json = JSON.parse(f);
+  Object.keys(json).forEach(function(key){
+    if(json[key].length > 0){
+      console.log(key);
+      var notCovered = {};
+      for(var i=0; i<json[key].length;i++ ){
+        var line = json[key][i]['lineNum'];
+        var range = json[key][i].lines[0].range;
+        notCovered[line] = range;
+      }
+      var file = fs.readFileSync(key, 'utf8')
+      file = file.split('\n');
+      for(var i=0; i<file.length; i++){
+        var line = file[i];
+        if(notCovered[i]){
+          console.log(pad((i+1).toString(), 6)+' '+uncovered(line));
+        }else{
+          console.log(pad((i+1).toString(), 6)+' '+covered(line));
+        }
+      };
+    }
+  });
+}
+
